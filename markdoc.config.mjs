7790b6543a851a0aff99861e548aea1f96967ca6
maui <hello@mauriciabad.com>
2023-12-18 18:18:48 +0100

    Display suits instead of emojis

--- markdoc.config.mjs
+++ markdoc.config.mjs
@@ -18,6 +18,58 @@ export default defineMarkdocConfig({
         },
       },
     },
+    suit: {
+      render: component('./src/components/markdown/Suit.astro'),
+      selfClosing: true,
+      attributes: {
+        id: {
+          type: Number,
+          matches: [
+            0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 111, 121, 131, 112, 122, 132,
+            113, 123, 133, 114, 124, 134, 115, 125, 135, 145,
+          ],
+          errorLevel: 'critical',
+        },
+        emoji: {
+          type: String,
+          matches: [
+            '🔥',
+            '💧',
+            '⭐',
+            '🍀',
+            '🍊',
+            '🍆',
+            '💭',
+            '🕷️',
+            '🌸',
+            '💎',
+            '💩',
+            '🗿',
+            '❤️',
+            '🔴',
+            '🟥',
+            '💙',
+            '🔵',
+            '🟦',
+            '💛',
+            '🟡',
+            '🟨',
+            '💚',
+            '🟢',
+            '🟩',
+            '🖤',
+            '⚫️',
+            '⬛️',
+            '🌈',
+          ],
+          errorLevel: 'critical',
+        },
+        size: {
+          type: Number,
+          errorLevel: 'critical',
+        },
+      },
+    },
     cardDistributionTable: {
       render: component(
         './src/components/markdown/CardDistributionTable.astro'
