2eb009dd41294d7af8d73ad1db75a525c6154ae6
maui <hello@mauriciabad.com>
2023-12-30 03:18:28 +0100

    Update description

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ![preview](/public/images/og.png)
 
-The DIY multideck is a deck of 160 poker-sized playing cards that lets you play many games by writing in the cards and using a bit of imagination.
+The DIY multideck is a deck of 162 playing cards (3 standard decks) that allows you to play hundreds of existing games, not only classic card games but also modern games that use components like boards or coins. The DIY multideck is ideal for traveling and prototyping new games.
 
 More details about the deck in: <https://diymultideck.mauri.app/>
 
--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = 'DIY Multideck'
 export const SITE_DESCRIPTION =
-  'The DIY multideck is a deck of 160 poker-sized playing cards that lets you play many games by writing in the cards and using a bit of imagination.'
+  'The DIY multideck is a deck of 162 playing cards (3 standard decks) that allows you to play hundreds of existing games, not only classic card games but also modern games that use components like boards or coins. The DIY multideck is ideal for traveling and prototyping new games.'
 export const MY_NAME = 'Maurici Abad Gutierrez'
 
 // setup in astro.config.mjs
