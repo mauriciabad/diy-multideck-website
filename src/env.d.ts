e4ad79eec12508f40cb43660517fa8498853a102
maui <hello@mauriciabad.com>
2025-01-02 04:11:24 +0100

    forgot to commit

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,25 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+import type { GameInfo } from './lib/schemas/game'
+import type { GamesJson } from './lib/schemas/gamesJson'
+
+declare module 'astro:content' {
+  interface ContentCollections {
+    'games-md': {
+      type: 'content'
+      schema: GameInfo
+    }
+    'games-json': {
+      type: 'data'
+      schema: GamesJson
+    }
+  }
+
+  type ContentEntryMap = {
+    'games-md': CollectionEntry<'games-md'>
+  }
+
+  type DataEntryMap = {
+    'games-json': CollectionEntry<'games-json'>
+  }
+}
--- src/lib/schemas/date.ts
+++ src/lib/schemas/date.ts
@@ -1,6 +1,6 @@
 import { z } from 'astro:content'
 
-export const dateSchema = z.date({
+export const dateSchema = z.coerce.date({
   required_error: 'Required frontmatter missing: date',
   invalid_type_error:
     'date must be written in yyyy-mm-dd format without quotes: For example, Jan 22, 2000 should be written as 2000-01-22.',
