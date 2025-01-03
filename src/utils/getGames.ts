4920b94dd1a3026ea51080c9caf8b1ed57073fd6
maui <hello@mauriciabad.com>
2025-01-03 04:24:13 +0100

    Fix load pages with json

---
+++ src/utils/getGames.ts
@@ -0,0 +1,12 @@
+import { getCollection } from 'astro:content'
+import uniqBy from 'lodash/uniqBy'
+
+export async function getGamesCollections() {
+  const jsonGames = (await getCollection('games-json')).map((g) => ({
+    slug: g.id,
+    ...g,
+  }))
+  const mdGames = await getCollection('games-md')
+  const games = uniqBy([...jsonGames, ...mdGames], 'slug')
+  return games
+}
