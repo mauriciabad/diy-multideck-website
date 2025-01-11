8f69cbe0592c5df2214696ae31f815f06c6e54aa
maui <hello@mauriciabad.com>
2025-01-12 00:08:03 +0100

    Better intersection

--- src/lib/schemas/gamesJson.ts
+++ src/lib/schemas/gamesJson.ts
@@ -2,9 +2,6 @@ import { z } from 'astro:content'
 import { gameSchema } from './game'
 import { gameMappingsSchema } from './gameMappingsSchema'
 
-export const gamesJsonSchema = z.intersection(
-  gameSchema.strip(),
-  gameMappingsSchema.strip()
-)
+export const gamesJsonSchema = gameSchema.merge(gameMappingsSchema).strict()
 
 export type GamesJson = z.infer<typeof gamesJsonSchema>
