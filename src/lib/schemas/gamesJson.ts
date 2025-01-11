9710320f7d773121d501e657b4e8fd0a1ba3970b
maui <hello@mauriciabad.com>
2025-01-12 00:03:20 +0100

    strip

--- src/lib/schemas/gamesJson.ts
+++ src/lib/schemas/gamesJson.ts
@@ -2,6 +2,9 @@ import { z } from 'astro:content'
 import { gameSchema } from './game'
 import { gameMappingsSchema } from './gameMappingsSchema'
 
-export const gamesJsonSchema = z.intersection(gameSchema, gameMappingsSchema)
+export const gamesJsonSchema = z.intersection(
+  gameSchema.strip(),
+  gameMappingsSchema.strip()
+)
 
 export type GamesJson = z.infer<typeof gamesJsonSchema>
